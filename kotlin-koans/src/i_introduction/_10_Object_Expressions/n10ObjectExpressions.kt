4663ca5e81c31c1a6e13c6414080683d01f6f1b3
Michael <michael@soapsurfer.de>
2018-04-28 14:46:54 -0300

    Added kotlin-koans tutorial

---
+++ kotlin-koans/src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
@@ -0,0 +1,31 @@
+package i_introduction._10_Object_Expressions
+
+import util.TODO
+import util.doc10
+import java.util.*
+import kotlin.Comparator
+
+fun todoTask10(): Nothing = TODO(
+        """
+        Task 10.
+        Read about object expressions that play the same role in Kotlin as anonymous classes in Java.
+
+        Add an object expression that provides a comparator to sort a list in a descending order using 'java.util.Collections' class.
+        In Kotlin you use Kotlin library extensions instead of java.util.Collections,
+        but this example is still a good demonstration of mixing Kotlin and Java code.
+    """,
+        documentation = doc10()
+)
+
+fun task10(): List<Int> {
+    val arrayList = arrayListOf(1, 5, 2)
+    Collections.sort(arrayList, object : Comparator<Int> {
+
+        override fun compare(a: Int, b: Int): Int {
+            if (a < b) return 1
+            else if (a > b) return -1
+            else return 0
+        }
+    })
+    return arrayList
+}
