602f5329b2b45b9367e9809bf2767d4aaaa53985
Michael <michael@soapsurfer.de>
2018-05-03 18:03:54 -0300

    completed properties

--- kotlin-koans/src/iv_properties/n33LazyProperty.kt
+++ kotlin-koans/src/iv_properties/n33LazyProperty.kt
@@ -3,11 +3,20 @@ package iv_properties
 import util.TODO
 
 class LazyProperty(val initializer: () -> Int) {
-    val lazy: Int = todoTask33()
+    val lazyProjection: Int? = null
+        get() {
+            if (field == null) {
+                field = initializer()
+            }
+            return field
+        }
+
+    val lazy: Int
+        get() = lazyProjection as Int // casting here is safe as we set lazyProjection in the getter
 }
 
 fun todoTask33(): Nothing = TODO(
-    """
+        """
         Task 33.
         Add a custom getter to make the 'lazy' val really lazy.
         It should be initialized by the invocation of 'initializer()'
@@ -15,5 +24,5 @@ fun todoTask33(): Nothing = TODO(
         You can add as many additional properties as you need.
         Do not use delegated properties yet!
     """,
-    references = { LazyProperty({ 42 }).lazy }
+        references = { LazyProperty({ 42 }).lazy }
 )
