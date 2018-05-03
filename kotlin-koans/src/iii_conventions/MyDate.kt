bfdeeda42603f588271a9c64ce9b0fdf35c6ea01
Michael <michael@soapsurfer.de>
2018-05-03 15:22:20 -0300

    completed conventions

--- kotlin-koans/src/iii_conventions/MyDate.kt
+++ kotlin-koans/src/iii_conventions/MyDate.kt
@@ -1,13 +1,56 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import java.util.*
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) {
+}
+
+operator fun MyDate.inc(): MyDate = this.nextDay()
+
+operator fun MyDate.plus(interval: TimeInterval): MyDate = this.addTimeIntervals(interval, 1)
+
+operator fun MyDate.plus(repeatedTimeInterval: RepeatedTimeInterval): MyDate =
+        this.addTimeIntervals(repeatedTimeInterval.interval, repeatedTimeInterval.reps)
+
+operator fun MyDate.compareTo(other: MyDate): Int = when {
+    this.year != other.year -> this.year - other.year
+    this.month != other.month -> this.month - other.month;
+//this.dayOfMonth != other.dayOfMonth -> this.dayOfMonth - other.dayOfMonth
+    else -> {
+        this.dayOfMonth - other.dayOfMonth
+    }
+}
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
 enum class TimeInterval {
+
     DAY,
     WEEK,
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+operator fun TimeInterval.times(reps: Int): RepeatedTimeInterval = RepeatedTimeInterval(this, reps)
+
+class RepeatedTimeInterval(val interval: TimeInterval, val reps: Int) {
+}
+
+class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> = MyDateIterator(this)
+}
+
+class MyDateIterator(val dateRange: DateRange) : Iterator<MyDate> {
+    var currentDate: MyDate = dateRange.start
+
+    override fun hasNext(): Boolean = dateRange.endInclusive >= currentDate
+
+    override fun next(): MyDate {
+        val currentVal = currentDate
+        currentDate++
+        return currentVal
+    }
+
+}
+
+operator fun DateRange.contains(date: MyDate): Boolean =
+        this.start <= date && this.endInclusive >= date
