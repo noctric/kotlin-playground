94e6caa71615c71befbdaae4d7e290fdfedc25fa
Michael <michael@soapsurfer.de>
2018-05-04 17:10:57 -0300

    completed generics chapter

--- kotlin-koans/src/vi_generics/n41GenericFunctions.kt
+++ kotlin-koans/src/vi_generics/n41GenericFunctions.kt
@@ -4,7 +4,7 @@ import util.TODO
 import java.util.*
 
 fun task41(): Nothing = TODO(
-    """
+        """
         Task41.
         Add a 'partitionTo' function that splits a collection into two collections according to a predicate.
         Uncomment the commented invocations of 'partitionTo' below and make them compile.
@@ -20,11 +20,25 @@ fun task41(): Nothing = TODO(
 )
 
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-    task41()
-//    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
 }
 
 fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-    task41()
-//    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z' }
 }
+
+fun <T, K : MutableCollection<T>> Collection<T>.partitionTo(firstList: K, secondList: K, predicate: (T) -> Boolean):
+        Pair<K, K> {
+
+    for (item in this) {
+        if (predicate(item)) {
+            firstList.add(item)
+        } else {
+            secondList.add(item)
+        }
+    }
+
+    return Pair(firstList, secondList)
+
+}
+
